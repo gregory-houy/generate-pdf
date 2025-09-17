ea721ff294156dced7be30058c9681af8e8c19f7
gregory-houy <houygregory2009@gmail.com>
2025-09-17 16:43:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ async function fetchAdherents() {
     try {
         const { data, error } = await supabase
             .from('adherents')
-            .select('nom, prenom, username, password, classe, code');
+            .select('nom, prenom, username, password, classe, code'); // Ajout des colonnes classe et code
         if (error) throw error;
         return data;
     } catch (err) {
@@ -30,7 +30,9 @@ async function generatePDFs() {
     const documentContent = [];
 
     adherents.forEach((adherent, index) => {
+        // Crée le contenu d'une seule page pour un adhérent
         const pageContent = [
+            // Contenu du document, stylisé pour correspondre à votre modèle
             { text: 'Lycée', style: 'header' },
             { text: 'CHÂTEAU BLANC', style: 'subheader' },
             { text: `Nom: ${adherent.nom}`, margin: [0, 20, 0, 5] },
@@ -39,6 +41,7 @@ async function generatePDFs() {
             { text: `Identifiant: ${adherent.username}`, margin: [0, 20, 0, 5] },
             { text: `Mot de passe: ${adherent.password}`, margin: [0, 0, 0, 5] },
             { text: `Code unique: ${adherent.code}`, margin: [0, 0, 0, 5] },
+            // S'il s'agit du dernier adhérent, ne pas ajouter de saut de page
             ...(index < adherents.length - 1 ? [{ text: '', pageBreak: 'after' }] : [])
         ];
         documentContent.push(...pageContent);
@@ -61,17 +64,13 @@ async function generatePDFs() {
         defaultStyle: {
             // S'assurer que le style par défaut utilise les polices que vous définissez
             font: 'Helvetica'
-        },
-        fonts: {
-            Helvetica: {
-                normal: 'https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/fonts/Helvetica/Helvetica.ttf',
-                bold: 'https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/fonts/Helvetica/Helvetica-Bold.ttf',
-                italics: 'https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/fonts/Helvetica/Helvetica-Oblique.ttf',
-                bolditalics: 'https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/fonts/Helvetica/Helvetica-BoldOblique.ttf'
-            }
         }
     };
 
+    // Définition explicite des polices pour le document
+    pdfMake.vfs = pdfFonts.pdfMake.vfs;
+    
+    // Crée le PDF et le télécharge sous le nom 'Fiches_Adherents.pdf'
     pdfMake.createPdf(docDefinition).download('Fiches_Adherents.pdf');
 }
 
