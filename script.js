f1bf778441e15d45a6c72b296f1422392ce35217
gregory-houy <houygregory2009@gmail.com>
2025-09-17 16:41:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Importation du client Supabase
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-// Assurez-vous d'avoir les bonnes valeurs ici
+// Mettez à jour ces valeurs avec vos propres clés
 const supabaseUrl = 'https://iarukjyswplvmtcxjtbx.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlhcnVranlzd3Bsdm10Y3hqdGJ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYwNTYwMjUsImV4cCI6MjA3MTYzMjAyNX0.jC33JZm5vwOROpxEMBCRQTaGwe-TF06fRMHg1UcoHxY';
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
@@ -10,7 +10,7 @@ async function fetchAdherents() {
     try {
         const { data, error } = await supabase
             .from('adherents')
-            .select('nom, prenom, username, password, classe, code'); // Ajout des colonnes classe et code
+            .select('nom, prenom, username, password, classe, code');
         if (error) throw error;
         return data;
     } catch (err) {
@@ -30,9 +30,7 @@ async function generatePDFs() {
     const documentContent = [];
 
     adherents.forEach((adherent, index) => {
-        // Crée le contenu d'une seule page pour un adhérent
         const pageContent = [
-            // Contenu du document, stylisé pour correspondre à votre modèle
             { text: 'Lycée', style: 'header' },
             { text: 'CHÂTEAU BLANC', style: 'subheader' },
             { text: `Nom: ${adherent.nom}`, margin: [0, 20, 0, 5] },
@@ -41,7 +39,6 @@ async function generatePDFs() {
             { text: `Identifiant: ${adherent.username}`, margin: [0, 20, 0, 5] },
             { text: `Mot de passe: ${adherent.password}`, margin: [0, 0, 0, 5] },
             { text: `Code unique: ${adherent.code}`, margin: [0, 0, 0, 5] },
-            // S'il s'agit du dernier adhérent, ne pas ajouter de saut de page
             ...(index < adherents.length - 1 ? [{ text: '', pageBreak: 'after' }] : [])
         ];
         documentContent.push(...pageContent);
@@ -62,11 +59,19 @@ async function generatePDFs() {
             }
         },
         defaultStyle: {
+            // S'assurer que le style par défaut utilise les polices que vous définissez
             font: 'Helvetica'
+        },
+        fonts: {
+            Helvetica: {
+                normal: 'https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/fonts/Helvetica/Helvetica.ttf',
+                bold: 'https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/fonts/Helvetica/Helvetica-Bold.ttf',
+                italics: 'https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/fonts/Helvetica/Helvetica-Oblique.ttf',
+                bolditalics: 'https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/fonts/Helvetica/Helvetica-BoldOblique.ttf'
+            }
         }
     };
 
-    // Crée le PDF et le télécharge sous le nom 'Fiches_Adherents.pdf'
     pdfMake.createPdf(docDefinition).download('Fiches_Adherents.pdf');
 }
 
